4c392b1733a94535506807417476cf611ebd9b55
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-07-09 17:40:02 +0200

    add test with instance Calculator and call method add() of Calculator and generate automaticly the class Calculator and it method add() to call method in class test and execute the test

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -7,8 +7,17 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAddTwoPositiveNumbers() {
+		// Arrange
+		int a = 2;
+		int b = 3;
+		Calculator calculator = new Calculator();
+		
+		// Act
+		int somme = calculator.add(a, b);
+
+		// Assert
+		assertEquals(5, somme);
 	}
 
 }
