72897a56cb1ab8cf676e42b3c8501b70f08fe892
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-07-09 20:17:43 +0200

    implement method add() and multiply()in class Calculator.java  to pass all tests of CalculatorTest.java

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -4,7 +4,12 @@ public class Calculator {
 
 	public int add(int a, int b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a + b;
+	}
+
+	public Integer multiply(int a, int b) {
+		// TODO Auto-generated method stub
+		return a * b;
 	}
 
 }
--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,23 +1,48 @@
 package com.openclassrooms.testing;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
 class CalculatorTest {
+	
+	// Arrange
+	private static Calculator calculator;
+	
+	@BeforeAll // avant l executions des test on instancie UNE seule fois la classe calculator (vs BeforeEach qui executerai la method à chaque test
+	private static void setUp() { // cette methode est static pour que java execute la methode a partir de la classe CalculatorTest meme avec une espace de nom de la classe et non d'une instance de cette classe
+		calculator = new Calculator();
+	}
 
 	@Test
 	void testAddTwoPositiveNumbers() {
+		
 		// Arrange
 		int a = 2;
 		int b = 3;
-		Calculator calculator = new Calculator();
-		
+		//Calculator calculator = new Calculator();
+			
 		// Act
 		int somme = calculator.add(a, b);
 
 		// Assert
 		assertEquals(5, somme);
+	}
+	
+	// Pour bien definir ce qu'on veut tester , il est recommandé de commencer a rebours par l etape Assert, puis Act, enfin Arrange
+	@Test
+	void testMultiplyTwoPositiveNumbers() {
+		
+		// Arrange
+		int a = 5;
+		int b = 4;
+		
+		// Act
+		Integer somme = calculator.multiply(a, b);
+		
+		// Assert
+		assertEquals(20, somme);
+		
 	}
 
 }
